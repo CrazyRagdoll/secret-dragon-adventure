c2972219597e71689d91a3255ed6c8119bac5344
Oliver Barron-Carter <olliebc@hotmail.com>
2014-05-07 14:13:57 +0100

    A few new rules. Added the person fact, a person can be talked_to when the player is at the perons location. list_stuff now looks through all facts and returns all matching facts. new fact response, which is written to the terminal when the player talks to that person.

--- adventure-game.pl
+++ adventure-game.pl
@@ -1,11 +1,23 @@
-start:-
-	write('You are an adventurer who has travelled far, you journey to defeat the largest dragon in the land, Mesanth Eater of Sheep, has brought you here.'), nl.
-
+%declare current_location as dynamic, so it can change.
 :- dynamic current_location/1.
 
+%initial world setup.
 	current_location(hamlet). %gridpos00
 	is_at(hamlet, sellsword).
+	is_at(hamlet, blacksmith).
 	coord(hamlet, '0,0').
+%define people.
+	person(sellsword).
+	person(blacksmith).
+
+%responses from NPCs
+	response(sellsword, 'So you''re the one looking for ol'' Mesanth aye? well, i too have quested to defeat the Beast, but i could not find him, all i found was what remains of the places it has visited. Do not make the same mistake i did.').
+
+%give back story and observe initial location.
+start:-
+	write('You are an adventurer who has travelled far, you journey to defeat the largest dragon in the land, Mesanth Eater of Sheep, has brought you here.'), nl,
+	observe.
+%observe current surroundings.
 observe:-
 	current_location(X),
 	coord(X, Y),
@@ -15,6 +27,16 @@ observe:-
 	write(Y), nl,
 	list_stuff(X).
 
+%list all things where the player currently is.
 list_stuff(X):-
 	is_at(X, THING),
-	write('you notice a '), write(THING), write(' is located here.'), nl.
+	write('you notice a '), write(THING), write(' is located here.'), nl, fail.
+list_stuff(X).
+
+%talk to X, X must be a person and be where the player is.
+talk_to(X):-
+	current_location(Z),
+	person(X),
+	is_at(Z, X),
+	response(X, T),
+	write(T), nl.
